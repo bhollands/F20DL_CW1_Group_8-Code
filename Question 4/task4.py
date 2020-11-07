43dde63ebea8ed389e8d9ce580cb4952e2ef783a
Perry <57232543+Perry1903@users.noreply.github.com>
2020-11-07 23:32:01 +0000

    Updating Question 4

--- Question 4/task4.py
+++ Question 4/task4.py
@@ -98,6 +98,7 @@ dataset1 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
 columns = [str(x) for x in range(5)]
 
 df1 = pd.DataFrame(dataset1, columns=columns)
+print(df1.head())
 df1.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_1.csv', index=False, header=False)
 
 l0 = []
@@ -155,10 +156,91 @@ for label in labels:
                     imgs[i][583],imgs[i][676],imgs[i][824],imgs[i][573],imgs[i][572]])
     i+=1
 
-print(l0)
 dataset2 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
 columns = [str(x) for x in range(10)]
 
 df2 = pd.DataFrame(dataset2, columns=columns)
 print(df2.head())
 df2.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_2.csv', index=False, header=False)
+
+l0 = []
+l1 = []
+l2 = []
+l3 = []
+l4 = []
+l5 = []
+l6 = []
+l7 = []
+l8 = []
+l9 = []
+
+# Top 20 features per class
+i = 0
+for label in labels:
+    if label[0] == '0':
+        l0.append([imgs[i][714],imgs[i][542],imgs[i][507],imgs[i][749],imgs[i][577],
+                    imgs[i][680],imgs[i][783],imgs[i][784],imgs[i][748],imgs[i][545],
+                    imgs[i][580],imgs[i][822],imgs[i][818],imgs[i][715],imgs[i][19],
+                    imgs[i][679],imgs[i][54],imgs[i][471],imgs[i][615],imgs[i][55]])
+
+    if label[0] == '1':
+        l1.append([imgs[i][542],imgs[i][646],imgs[i][541],imgs[i][752],imgs[i][577],
+                    imgs[i][576],imgs[i][717],imgs[i][611],imgs[i][786],imgs[i][506],
+                    imgs[i][682],imgs[i][793],imgs[i][681],imgs[i][751],imgs[i][645],
+                    imgs[i][545],imgs[i][819],imgs[i][792],imgs[i][783],imgs[i][510]])
+
+    if label[0] == '2':
+        l2.append([imgs[i][319],imgs[i][284],imgs[i][285],imgs[i][320],imgs[i][249],
+                    imgs[i][318],imgs[i][283],imgs[i][248],imgs[i][353],imgs[i][247],
+                    imgs[i][354],imgs[i][214],imgs[i][250],imgs[i][317],imgs[i][421],
+                    imgs[i][352],imgs[i][386],imgs[i][286],imgs[i][282],imgs[i][213]])
+
+    if label[0] == '3':
+        l3.append([imgs[i][644],imgs[i][678],imgs[i][679],imgs[i][713],imgs[i][645],
+                    imgs[i][643],imgs[i][714],imgs[i][748],imgs[i][609],imgs[i][610],
+                    imgs[i][646],imgs[i][680],imgs[i][712],imgs[i][677],imgs[i][783],
+                    imgs[i][749],imgs[i][753],imgs[i][829],imgs[i][752],imgs[i][682]])
+                    
+    if label[0] == '4':
+        l4.append([imgs[i][542],imgs[i][1140],imgs[i][1141],imgs[i][543],imgs[i][1109],
+                    imgs[i][1110],imgs[i][1175],imgs[i][1142],imgs[i][1111],imgs[i][1096],
+                    imgs[i][1201],imgs[i][1108],imgs[i][1143],imgs[i][1200],imgs[i][507],
+                    imgs[i][1146],imgs[i][1095],imgs[i][1097],imgs[i][1145],imgs[i][1144]])
+
+    if label[0] == '5':
+        l5.append([imgs[i][677],imgs[i][642],imgs[i][861],imgs[i][826],imgs[i][406],
+                    imgs[i][371],imgs[i][683],imgs[i][719],imgs[i][370],imgs[i][643],
+                    imgs[i][684],imgs[i][401],imgs[i][405],imgs[i][570],imgs[i][791],
+                    imgs[i][400],imgs[i][608],imgs[i][756],imgs[i][535],imgs[i][726]])
+
+    if label[0] == '6':
+        l6.append([imgs[i][439],imgs[i][440],imgs[i][404],imgs[i][405],imgs[i][403],
+                    imgs[i][618],imgs[i][582],imgs[i][617],imgs[i][583],imgs[i][438],
+                    imgs[i][678],imgs[i][402],imgs[i][653],imgs[i][441],imgs[i][677],
+                    imgs[i][547],imgs[i][401],imgs[i][643],imgs[i][652],imgs[i][856]])
+
+    if label[0] == '7':
+        l7.append([imgs[i][648],imgs[i][583],imgs[i][642],imgs[i][405],imgs[i][607],
+                    imgs[i][613],imgs[i][406],imgs[i][618],imgs[i][548],imgs[i][441],
+                    imgs[i][573],imgs[i][676],imgs[i][369],imgs[i][677],imgs[i][619],
+                    imgs[i][440],imgs[i][649],imgs[i][726],imgs[i][370],imgs[i][547]])
+
+    if label[0] == '8':
+        l8.append([imgs[i][406],imgs[i][642],imgs[i][405],imgs[i][677],imgs[i][583],
+                    imgs[i][607],imgs[i][370],imgs[i][548],imgs[i][441],imgs[i][676],
+                    imgs[i][711],imgs[i][573],imgs[i][619],imgs[i][572],imgs[i][400],
+                    imgs[i][641],imgs[i][369],imgs[i][726],imgs[i][608],imgs[i][401]])
+
+    if label[0] == '9':
+        l9.append([imgs[i][642],imgs[i][677],imgs[i][607],imgs[i][618],imgs[i][860],
+                    imgs[i][583],imgs[i][676],imgs[i][824],imgs[i][573],imgs[i][572],
+                    imgs[i][582],imgs[i][641],imgs[i][401],imgs[i][825],imgs[i][440],
+                    imgs[i][547],imgs[i][608],imgs[i][711],imgs[i][859],imgs[i][653]])
+    i+=1
+
+dataset3 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
+columns = [str(x) for x in range(20)]
+
+df3 = pd.DataFrame(dataset3, columns=columns)
+print(df3.head())
+df3.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_3.csv', index=False, header=False)
