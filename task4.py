3296687c45f2cc17a7dc3e59554a4902c2effab6
Perry <57232543+Perry1903@users.noreply.github.com>
2020-11-07 21:24:19 +0000

    Updating Task 4

--- task4.py
+++ task4.py
@@ -6,35 +6,36 @@ assert sys.version_info >= (3, 5)
 # import sklearn
 # assert sklearn.__version__ >= "0.20"
 
-# # Common imports
+# Common imports
 import numpy as np
 # import os
 # import tarfile
 # import urllib
 # import pandas as pd
+import pandas as pd
 import csv
 import math
-
+from scipy.stats import pearsonr
+  
 # To plot pretty figures
 # import matplotlib as mpl
-# import matplotlib.pyplot as plt
 
 # Import the data here
 img_data = "x_train_gr_smpl_random_reduced.csv"
 lbl_data = "y_train_smpl_random.csv"
-# file = "test.csv"
 
-# Task 4
-# 1. Filter through the dataset and group all images to the relevant classes
-# 2. For each class, record the first 10 pixels in order of the absolute correlation value, for each street sign
-# 3. Contain data for use in task 5
+f = open(lbl_data, "r", encoding='utf-8')
+labels = csv.reader(f, quotechar='"')
 
-# Step 1: Index of image in x_train corresponds to index of its class label in y_train, so:
-#         - Locate and store the positions of each class label in y_train in separate arrays
-#         - Filter through x_train data to locate and store the correct images for each class into their class array, using the positions
+f = open(img_data, "r", encoding='utf-8')
+imgs = csv.reader(f, quotechar='"')
+imgs = list(imgs)
 
-# Step 2: For each class, find and record the top 10 pixels of each image, in order of absolute correlation value
-
+# Fixing problem where first image contains some float values, convert to int and back to string
+imgs[0] = [float(x) for x in imgs[0]]
+imgs[0] = [int(x) for x in imgs[0]]
+imgs[0] = [str(x) for x in imgs[0]]
+  
 l0 = []
 l1 = []
 l2 = []
@@ -46,66 +47,43 @@ l7 = []
 l8 = []
 l9 = []
 
-f = open(img_data, "r", encoding='utf-8')
-imgs = csv.reader(f, quotechar='"')
-imgs = list(imgs)
+# Class 0 - 714,542,507,749,577,680,783,784,748,545,580,822,818,715,19,679,54,471,615,55
+# Class 1 - 542,646,541,752,577,576,717,611,786,506,682,793,681,751,645,545,819,792,783,510
+# Class 2 - 319,284,285,320,249,318,283,248,353,247,354,214,250,317,421,352,386,286,282,213
+# Class 3 - 644,678,679,713,645,643,714,748,609,610,646,680,712,677,783,749,753,829,752,682
+# Class 4 - 542,1140,1141,543,1109,1110,1175,1142,1111,1096,1201,1108,1143,1200,507,1146,1095,1097,1145,1144
+# Class 5 - 677,642,861,826,406,371,683,719,370,643,684,401,405,570,791,400,608,756,535,726
+# Class 6 - 439,440,404,405,403,618,582,617,583,438,678,402,653,441,677,547,401,643,652,856
+# Class 7 - 648,583,642,405,607,613,406,618,548,441,573,676,369,677,619,440,649,726,370,547
+# Class 8 - 406,642,405,677,583,607,370,548,441,676,711,573,619,572,400,641,369,726,608,401
+# Class 9 - 642,677,607,618,860,583,676,824,573,572,582,641,401,825,440,547,608,711,859,653
 
-# Fixing problem where first image contains some float values, convert to int
-imgs[0] = [float(x) for x in imgs[0]]
-imgs[0] = [int(x) for x in imgs[0]]
-
-# Convert all the image data from string to int
-for i in range(9690):
-    imgs[i] = [int(x) for x in imgs[i]]
-
-f = open(lbl_data, "r", encoding='utf-8')
-labels = csv.reader(f, quotechar='"')
-
+# Top 5 features per class  
 i = 0
 for label in labels:
     if label[0] == '0':
-        l0.append(imgs[i])
+        l0.append([imgs[i][714],imgs[i][542],imgs[i][507],imgs[i][749],imgs[i][577]])
     if label[0] == '1':
-        l1.append(imgs[i])
+        l1.append([imgs[i][542],imgs[i][646],imgs[i][541],imgs[i][752],imgs[i][577]])
     if label[0] == '2':
-        l2.append(imgs[i])
+        l2.append([imgs[i][319],imgs[i][284],imgs[i][285],imgs[i][320],imgs[i][249]])
     if label[0] == '3':
-        l3.append(imgs[i])
+        l3.append([imgs[i][644],imgs[i][678],imgs[i][679],imgs[i][713],imgs[i][645]])
     if label[0] == '4':
-        l4.append(imgs[i])
+        l4.append([imgs[i][542],imgs[i][1140],imgs[i][1141],imgs[i][543],imgs[i][1109]])
     if label[0] == '5':
-        l5.append(imgs[i])
+        l5.append([imgs[i][677],imgs[i][642],imgs[i][861],imgs[i][826],imgs[i][406]])
     if label[0] == '6':
-        l6.append(imgs[i])
+        l6.append([imgs[i][439],imgs[i][440],imgs[i][404],imgs[i][405],imgs[i][403]])
     if label[0] == '7':
-        l7.append(imgs[i])
+        l7.append([imgs[i][648],imgs[i][583],imgs[i][642],imgs[i][405],imgs[i][607]])
     if label[0] == '8':
-        l8.append(imgs[i])
+        l8.append([imgs[i][406],imgs[i][642],imgs[i][405],imgs[i][677],imgs[i][583]])
     if label[0] == '9':
-        l9.append(imgs[i])
+        l9.append([imgs[i][642],imgs[i][677],imgs[i][607],imgs[i][618],imgs[i][860]])
     i+=1
 
-class0 = np.array(l0)
-class1 = np.array(l1)
-class2 = np.array(l2)
-class3 = np.array(l3)
-class4 = np.array(l4)
-class5 = np.array(l5)
-class6 = np.array(l6)
-class7 = np.array(l7)
-class8 = np.array(l8)
-class9 = np.array(l9)
-
-print(class0[0])
-
-# For each image in each class, find the top 10 pixels correlating with the class
-
-
-
-
-# feature = classx[0][i]
-# corr = pearsonr(list(feature),list(x))
-# corrs0.append((float(feature), corr))
-
-# corrs0 = sorted(corrs0, key=float)
-# corrs0 = corrs0[:10]
+dataset1 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
+columns = [str(x) for x in range(5)]
+df1 = pd.DataFrame(dataset1, columns=columns)
+df1.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_1.csv', index=False, header=False)
