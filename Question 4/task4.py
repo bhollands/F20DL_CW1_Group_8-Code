e5651bb7f011a51af6bdbf70e5af063a7b361fd3
Perry <57232543+Perry1903@users.noreply.github.com>
2020-11-07 23:00:08 +0000

    Updating Question 4

--- Question 4/task4.py
+++ Question 4/task4.py
@@ -19,6 +19,7 @@ from scipy.stats import pearsonr
   
 # To plot pretty figures
 # import matplotlib as mpl
+# import matplotlib.pyplot as plt
 
 # Import the data here
 img_data = "x_train_gr_smpl_random_reduced.csv"
@@ -26,6 +27,7 @@ lbl_data = "y_train_smpl_random.csv"
 
 f = open(lbl_data, "r", encoding='utf-8')
 labels = csv.reader(f, quotechar='"')
+labels = list(labels)
 
 f = open(img_data, "r", encoding='utf-8')
 imgs = csv.reader(f, quotechar='"')
@@ -63,27 +65,100 @@ i = 0
 for label in labels:
     if label[0] == '0':
         l0.append([imgs[i][714],imgs[i][542],imgs[i][507],imgs[i][749],imgs[i][577]])
+
     if label[0] == '1':
         l1.append([imgs[i][542],imgs[i][646],imgs[i][541],imgs[i][752],imgs[i][577]])
+
     if label[0] == '2':
         l2.append([imgs[i][319],imgs[i][284],imgs[i][285],imgs[i][320],imgs[i][249]])
+
     if label[0] == '3':
         l3.append([imgs[i][644],imgs[i][678],imgs[i][679],imgs[i][713],imgs[i][645]])
+
     if label[0] == '4':
         l4.append([imgs[i][542],imgs[i][1140],imgs[i][1141],imgs[i][543],imgs[i][1109]])
+
     if label[0] == '5':
         l5.append([imgs[i][677],imgs[i][642],imgs[i][861],imgs[i][826],imgs[i][406]])
+
     if label[0] == '6':
         l6.append([imgs[i][439],imgs[i][440],imgs[i][404],imgs[i][405],imgs[i][403]])
+
     if label[0] == '7':
         l7.append([imgs[i][648],imgs[i][583],imgs[i][642],imgs[i][405],imgs[i][607]])
+
     if label[0] == '8':
         l8.append([imgs[i][406],imgs[i][642],imgs[i][405],imgs[i][677],imgs[i][583]])
+
     if label[0] == '9':
         l9.append([imgs[i][642],imgs[i][677],imgs[i][607],imgs[i][618],imgs[i][860]])
     i+=1
 
 dataset1 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
 columns = [str(x) for x in range(5)]
+
 df1 = pd.DataFrame(dataset1, columns=columns)
 df1.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_1.csv', index=False, header=False)
+
+l0 = []
+l1 = []
+l2 = []
+l3 = []
+l4 = []
+l5 = []
+l6 = []
+l7 = []
+l8 = []
+l9 = []
+
+# Top 10 features per class
+i = 0
+for label in labels:
+    if label[0] == '0':
+        l0.append([imgs[i][714],imgs[i][542],imgs[i][507],imgs[i][749],imgs[i][577],
+                    imgs[i][680],imgs[i][783],imgs[i][784],imgs[i][748],imgs[i][545]])
+
+    if label[0] == '1':
+        l1.append([imgs[i][542],imgs[i][646],imgs[i][541],imgs[i][752],imgs[i][577],
+                    imgs[i][576],imgs[i][717],imgs[i][611],imgs[i][786],imgs[i][506]])
+
+    if label[0] == '2':
+        l2.append([imgs[i][319],imgs[i][284],imgs[i][285],imgs[i][320],imgs[i][249],
+                    imgs[i][318],imgs[i][283],imgs[i][248],imgs[i][353],imgs[i][247]])
+
+    if label[0] == '3':
+        l3.append([imgs[i][644],imgs[i][678],imgs[i][679],imgs[i][713],imgs[i][645],
+                    imgs[i][643],imgs[i][714],imgs[i][748],imgs[i][609],imgs[i][610]])
+                    
+    if label[0] == '4':
+        l4.append([imgs[i][542],imgs[i][1140],imgs[i][1141],imgs[i][543],imgs[i][1109],
+                    imgs[i][1110],imgs[i][1175],imgs[i][1142],imgs[i][1111],imgs[i][1096]])
+
+    if label[0] == '5':
+        l5.append([imgs[i][677],imgs[i][642],imgs[i][861],imgs[i][826],imgs[i][406],
+                    imgs[i][371],imgs[i][683],imgs[i][719],imgs[i][370],imgs[i][643]])
+
+    if label[0] == '6':
+        l6.append([imgs[i][439],imgs[i][440],imgs[i][404],imgs[i][405],imgs[i][403],
+                    imgs[i][618],imgs[i][582],imgs[i][617],imgs[i][583],imgs[i][438]])
+
+    if label[0] == '7':
+        l7.append([imgs[i][648],imgs[i][583],imgs[i][642],imgs[i][405],imgs[i][607],
+                    imgs[i][613],imgs[i][406],imgs[i][618],imgs[i][548],imgs[i][441]])
+
+    if label[0] == '8':
+        l8.append([imgs[i][406],imgs[i][642],imgs[i][405],imgs[i][677],imgs[i][583],
+                    imgs[i][607],imgs[i][370],imgs[i][548],imgs[i][441],imgs[i][676]])
+
+    if label[0] == '9':
+        l9.append([imgs[i][642],imgs[i][677],imgs[i][607],imgs[i][618],imgs[i][860],
+                    imgs[i][583],imgs[i][676],imgs[i][824],imgs[i][573],imgs[i][572]])
+    i+=1
+
+print(l0)
+dataset2 = l0+l1+l2+l3+l4+l5+l6+l7+l8+l9
+columns = [str(x) for x in range(10)]
+
+df2 = pd.DataFrame(dataset2, columns=columns)
+print(df2.head())
+df2.to_csv('/Users/Perry/Documents/GitHub/F20DL_CW1_Group_8-Code/Question 5/train_sampl_2.csv', index=False, header=False)
